2691af82bcd7ed338ff005b61e849ba5ef575bdc
Shahzad Ahmad <atifali29786@gmail.com>
2024-05-28 15:25:48 +0500

    created multiple fields in the same metabox using an array

--- public/wp-content/plugins/advThemeMang/inc/Base/TestimonialControler.php
+++ public/wp-content/plugins/advThemeMang/inc/Base/TestimonialControler.php
@@ -79,26 +79,53 @@ class TestimonialControler
 
         function addMetaBoxes()
         {
-            add_meta_box( 'advThemeMang_author_name', 'Author', [$this, 'advThemeMang_author_name_callback'], 'Testimonial', 'normal', 'default' );
+            add_meta_box( 'advThemeMang_testimonial_options_metabox', 'Testimonial Options', [$this, 'advThemeMang_testimonial_options_metabox_callback'], 'Testimonial', 'normal', 'default' );
         }
-        function advThemeMang_author_name_callback( $post )
+        function advThemeMang_testimonial_options_metabox_callback( $post )
         {
             
-            wp_nonce_field( 'advThemeMang_author_name_nonce_id', 'advThemeMang_author_name_nonce' );
-            $author_name= get_post_meta( $post->ID, '_advThemeMang_author_name_key', true );
+            wp_nonce_field( 'advThemeMang_testimonial_options_metabox_nonce_id', 'advThemeMang_testimonial_options_metabox_nonce' );
+            $testimonial_options = get_post_meta( $post->ID, '_advThemeMang_testimonial_options_metabox_key', true );
+
+            $author_name = isset( $testimonial_options['author_name'] ) ? $testimonial_options['author_name'] : '';
+            $author_email = isset( $testimonial_options['author_email'] ) ? $testimonial_options['author_email'] : '';
+            $approved = isset( $testimonial_options['approved'] ) ? $testimonial_options['approved'] : '';
+            $featured = isset( $testimonial_options['featured'] ) ? $testimonial_options['featured'] : '';
+            
+                     
+           
             ?>
 <p>
     <label for="advThemeMang_author_name">Author Name</label>
     <input type="text" name="advThemeMang_author_name" id="advThemeMang_author_name" value="<?php echo $author_name; ?>" />
 </p>
+<p>
+    <label for="advThemeMang_author_email">Author Email</label>
+    <input type="email" name="advThemeMang_author_email" id="advThemeMang_author_email" value="<?php echo $author_email; ?>" />
+</p>
+<div class="toggle">
+
+    <strong>Approved</strong><input type="checkbox" id="advThemeMang_approved" name="advThemeMang_approved" value="1"
+        class="toggle__checkbox_input" <?php echo $approved ? 'checked' : ''; ?> />
+    <div class="toggle__background_container"><label for="advThemeMang_approved" class="toggle__label"></label></div>
+</div>';
+<div class="toggle">
+
+    <strong>Featured</strong><input type="checkbox" id="advThemeMang_featured" name="advThemeMang_featured" value="1"
+        class="toggle__checkbox_input" <?php echo $featured ? 'checked' : ''; ?> />
+    <div class="toggle__background_container"><label for="advThemeMang_featured" class="toggle__label"></label></div>
+</div>';
+
+
 <?php
         }
 
+     
         function save_author_metabox( $post_id )
         {
             
             
-            if ( ! isset( $_POST['advThemeMang_author_name_nonce'] ) || ! wp_verify_nonce( $_POST['advThemeMang_author_name_nonce'], 'advThemeMang_author_name_nonce_id' ) ) {
+            if ( ! isset( $_POST['advThemeMang_testimonial_options_metabox_nonce'] ) || ! wp_verify_nonce( $_POST['advThemeMang_testimonial_options_metabox_nonce'], 'advThemeMang_testimonial_options_metabox_nonce_id' ) ) {
                 return;
             }
             if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
@@ -107,8 +134,15 @@ class TestimonialControler
             if ( ! current_user_can( 'edit_post', $post_id ) ) {
                 return;
             }
-            if ( isset( $_POST['advThemeMang_author_name'] ) ) {
-                update_post_meta( $post_id, '_advThemeMang_author_name_key', sanitize_text_field( $_POST['advThemeMang_author_name'] ) );
+            if ( isset( $_POST['advThemeMang_testimonial_options_metabox'] ) ) {
+                return;
             }
+            $data = [
+                'author_name' => sanitize_text_field($_POST['advThemeMang_author_name']),
+                'author_email' =>sanitize_text_field( $_POST['advThemeMang_author_email']),
+                'approved' => isset($_POST['advThemeMang_approved']) ? $_POST['advThemeMang_approved'] : 0,
+                'featured' => isset($_POST['advThemeMang_featured']) ? $_POST['advThemeMang_featured'] : 0,
+            ];
+            update_post_meta( $post_id, '_advThemeMang_testimonial_options_metabox_key', $data);
         }
     }
